50c78a532541fbf88293846dc586532f290d4af5
Kunal <kunalaryad@gmail.com>
2025-04-26 13:02:53 +0530

    Divisor of Number

---
+++ Step-1 : Learn the basics/Lec 4: Know Basic Maths/06_divisor_of_num.py
@@ -0,0 +1,32 @@
+# You are given an integer n. You need to find all the divisors of n.
+# Return all the divisors of n as an array or list in a sorted order.
+
+# A number which completely divides another number is called it's divisor.
+
+# Brute Force
+# Time Complexity - O(n)
+
+def all_divisor(n):
+    temp = n
+    divisor = []
+    while temp > 0:
+        if n % temp == 0:
+            divisor.append(temp)
+        temp -= 1
+    return divisor[::-1]
+
+print(all_divisor(12))
+
+# Optimised - 1 ( Iterate till square root of the number )
+
+def all_divisor_op(n):
+    divisor = set()
+    
+    for i in range(1,int(n * 0.5) + 1):
+        if n % i == 0:
+            divisor.add(i)
+            divisor.add(n // i)
+    
+    return sorted(divisor)
+
+print(all_divisor_op(12))
